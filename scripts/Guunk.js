2c176f009098c6dc6fafb96f885e09427ec615da
Hansen Shi <hansens@udel.edu>
2020-03-06 13:22:04 -0500

    keylisteners

--- scripts/Guunk.js
+++ scripts/Guunk.js
@@ -1,1 +1,41 @@
-var context, controller, boi, Gameloop;
+const action = {
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    SPACE: 32,
+};
+
+let keys = {};
+document.addEventListener("keydown", function (event) {
+    if (
+        event.keyCode == action.RIGHT ||
+        event.keyCode == action.LEFT ||
+        event.keyCode == action.SPACE ||
+        event.keyCode == action.UP 
+        ) {
+        keys[event.keyCode] = true;
+    }
+});
+
+document.addEventListener("keyup", function (event) {
+    if (
+        event.keyCode == action.RIGHT ||
+        event.keyCode == action.LEFT ||
+        event.keyCode == action.SPACE ||
+        event.keyCode == action.UP 
+        ) {
+        keys[event.keyCode] = false;
+    }
+});
+
+$(document).ready(function () {
+    
+});
+
+function loop() {
+    if (keys[action.RIGHT]) {
+
+    }
+}
+
+
