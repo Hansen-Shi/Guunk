a685d6d97eaf1737299739bdf568443a15435881
Seth <sethp@udel.edu>
2020-03-07 12:38:10 -0500

    Reformatted Guunk.js into a function

--- scripts/Guunk.js
+++ scripts/Guunk.js
@@ -1,54 +1,75 @@
-let locX = 0;
-let locY = 0;
-let velocX = 0;
-let velocY = 0;
-let grav = 1;
+var Guunk = function() {
+    var self=this;
+    screen_w = screen.width;
+    screen_h = screen.height;
+    this.options={
+        width: screen_w,
+        height: screen_h,
+        gravity: 1,
 
-screen_w = screen.width;
-screen_h = screen.height;
-
-const action = {
-    LEFT: 37,
-    RIGHT: 39,
-    UP: 38,
-    SPACE: 32,
-};
-
-let keys = {};
-document.addEventListener("keydown", function (event) {
-    if (
-        event.keyCode == action.RIGHT ||
-        event.keyCode == action.LEFT ||
-        event.keyCode == action.SPACE ||
-        event.keyCode == action.UP
-    ) {
-        keys[event.keyCode] = true;
     }
-});
+    let locX = 0;
+    let locY = 0;
+    let velocX = 0;
+    let velocY = 0;
 
-document.addEventListener("keyup", function (event) {
-    if (
-        event.keyCode == action.RIGHT ||
-        event.keyCode == action.LEFT ||
-        event.keyCode == action.SPACE ||
-        event.keyCode == action.UP
-    ) {
-        keys[event.keyCode] = false;
+    this.player = new Player();
+
+    const action = {
+        LEFT: 37,
+        RIGHT: 39,
+        UP: 38,
+        SPACE: 32,
+    };
+
+    let keys = {};
+    document.addEventListener("keydown", function (event) {
+        /*if (
+            event.keyCode == action.RIGHT ||
+            event.keyCode == action.LEFT ||
+            event.keyCode == action.SPACE ||
+            event.keyCode == action.UP
+        ) {
+            keys[event.keyCode] = true;
+        }*/
+        if (event.keyCode == action.RIGHT) {
+            locX += 1;
+            player.x += 1;
+        }
+        if (event.keyCode == action.LEFT) {
+            locX -= 1;
+        }
+        if (event.keyCode == action.UP) {
+            locY += 1;
+        }
+        console.log(locX, locY);
+    });
+
+    document.addEventListener("keyup", function (event) {
+        if (
+            event.keyCode == action.RIGHT ||
+            event.keyCode == action.LEFT ||
+            event.keyCode == action.SPACE ||
+            event.keyCode == action.UP
+        ) {
+            keys[event.keyCode] = false;
+        }
+    });
+
+    function reset(document) {
+        document.style.left = screen_w * .5 + "px";
+        document.style.top = screen_h * .5 + "px";
+        document.style.width = 100 + "px";
+        document.style.height = 100 + "px";
     }
-});
 
-function reset(document) {
-    document.style.left = screen_w * .5 + "px";
-    document.style.top = screen_h * .5 + "px";
-    document.style.width = 100 + "px";
-    document.style.height = 100 + "px";
+    $(document).ready(function () {
+        const player = document.getElementById("slime");
+        reset(player);
+    });
 }
 
-$(document).ready(function () {
-    const player = document.getElementById("slime");
-    reset(player);
-});
 
+var Player = function(){
 
-
-
+}
