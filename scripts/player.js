3c16d5475aa6f5b0abb6e0c5c3c2d697942f612d
Hansen Shi <hansens@udel.edu>
2020-03-29 17:21:42 -0400

    blah

--- scripts/player.js
+++ scripts/player.js
@@ -4,35 +4,43 @@ var Player = function(){
     this.locY = 0;
     this.velocX = 0;
     this.velocY = 0;
+    this.grav = .07;
+    this.gravSpd = 0;
 
     this.initilize=function(){
         this.locX = 100;
-        this.locY = 500;
+        this.locY = 525;
         this.velocX = 0;
         this.velocY = 0;
     }
 
     this.update = function(keys){
-        if(keys.left){
+        if (keys.left){
             $('#slime').css('-transform', 'scaleX(1)');
             if(this.velocX > -5){
                 this.velocX -= 2;
             }
-            if(this.velocX < 0){
+            /*if(this.velocX < 0){
                 this.velocX += 1;
-            }
+            }*/
             this.locX+=this.velocX;
         }
 
-        if(keys.right){
+        if (keys.right){
             $('#slime').css('-transform', 'scaleX(-1)');
             if(this.velocX < 5){
                 this.velocX += 2;
             }
-            if(this.velocX > 0){
+            /*if(this.velocX > 0){
                 this.velocX -= 1;
-            }
+            }*/
             this.locX+=this.velocX;
+        }
+
+        if (keys.up){
+            this.gravSpd += this.grav;
+            this.velocY = -14;
+            this.locY += this.velocY + this.gravSpd;
         }
         console.log(this.locX, this.locY);
     };
